6a260bac0b9e584aa61d728460d923aba6335079
Cartmanis <VShmelcer@gmail.com>
2018-08-21 22:40:13 +0700

    4 learn

--- 4/gulp/gulpfile.js
+++ 4/gulp/gulpfile.js
@@ -4,17 +4,21 @@ const autoprefixer = require('gulp-autoprefixer'); //модуль автопре
 const cleanCSS = require('gulp-clean-css'); //модуль минификации css файлов
 const browserSync = require('browser-sync').create(); //модуль синхронизации кода с браузером. -чтобы постоянно не нажимать F5
 const sourcemaps = require('gulp-sourcemaps'); //модуль для удобной работы в браузере с минифицированными файлами
-const media = require('gulp-group-css-media-queries'); //модуль media из ДЗ
+const media = require('gulp-group-css-media-queries'); //модуль media из ДЗ-
+//групируем все media запросы с разных мест в одно
+const prepoc = require('gulp-less');
 
 //В этом объекте храним повторяющиеся пути нужные для gulp
 const config = {
     src: './src/',
     css: {
-        src: '/precss/**/*.css',
+        watch: '/precss/**/*.less', //следим за всеми файлами
+        //билдим только основной файл styles.less( который подключает вспомогательные файлы)
+        src: '/precss/styles.less',
         dest: '/css'
     },
     html: {
-        src: 'index.html', //Дз
+        src: '/index.html', //Дз
     }
 };
 
@@ -25,11 +29,12 @@ gulp.task('build', function () {
     //Запускаем gulp - **/ *.css означает, что мы ещем все вложенные папки */ 
     gulp.src(config.src + config.css.src)
         .pipe(sourcemaps.init())
+        .pipe(prepoc())
+        .pipe(media()) //использование media из Д/З 
         .pipe(autoprefixer({ //pipe автопрефикса(команды можно найти в документации gulp)
             browsers: ['> 0.1%'],
             cascade: false
         }))
-        .pipe(media()) //использование media из Д/З
         .pipe(cleanCSS())
         .pipe(sourcemaps.write('.')) //вызываем до dest
         .pipe(gulp.dest(config.src + config.css.dest)) //указываем куда мы получим обработанные файлы        
@@ -48,19 +53,18 @@ gulp.task('htmlWatch', function () {
         }));
 });
 
-
-gulp.task('browserSync', function () {
-    browserSync.init({ //обратить внимание, что команда не gulp, а browserSync
-        server: {
-            baseDir: config.src //путь где лежит файл index.html
-        }
-    })
-})
-
 //чтобы не запускать и watch и browserSync, можно во втором параметре 
 // в массиве указать те задачи, которые нужно запустить до watch
 gulp.task('watch', ['browserSync'], function () {
     //при изменении в любом css файле запускаем task 'build'
-    gulp.watch(config.src + config.css.src, ['build']);
-    gulp.watch(config.src + config.html.src, ['htmlWatch']);
+    gulp.watch(config.src + config.css.watch, ['build']);
+    gulp.watch(config.src + config.html.src, browserSync.reload);
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: config.src
+        }
+    });
 });
